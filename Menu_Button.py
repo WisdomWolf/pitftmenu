20c61430039c444f28dab268319a1df81477d297
WisdomWolf <WisdomWolf@gmail.com>
2015-09-16 14:20:46 -0400

    further herpaderp correction

--- Menu_Button.py
+++ Menu_Button.py
@@ -21,7 +21,7 @@ class Menu_Button:
         else:
             self.action()
             
-    def matches_touch(touch_pos):
+    def matches_touch(self, touch_pos):
         touch_x = touch_pos[0]
         touch_y = touch_pos[1]
         if self.x_pos <= touch_x <= (self.x_pos + self.width) and self.y_pos <= touch_y <= (self.y_pos + self.height):
