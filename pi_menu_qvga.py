b961727ded90b4a2c48e06651bd39025f2355b44
WisdomWolf <WisdomWolf@gmail.com>
2015-09-16 20:07:47 -0400

    adjusted clock to 12-hour clock with no seconds

--- pi_menu_qvga.py
+++ pi_menu_qvga.py
@@ -175,7 +175,7 @@ make_button("   Shutdown", 170, 165, 50, 145, Color.blue, shutdown, "Shutting Do
 def refresh_screen():
     
     date_string = time.strftime('%a %b %d, %Y')
-    time_string = time.strftime('%H:%M:%S')
+    time_string = time.strftime('%I:%M %p')
     
     screen.fill(Color.black, time_rect)
     make_label(date_string, 20, 10, 36, Color.blue)
